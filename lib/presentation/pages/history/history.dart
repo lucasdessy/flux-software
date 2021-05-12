dcce0b06ccb195224d055879a2c5ea84502b0dc4
lucasdessy <lucasedu49@gmail.com>
2021-05-12 14:12:16 -0300

    added mock infos to history page

--- lib/presentation/pages/history/history.dart
+++ lib/presentation/pages/history/history.dart
@@ -1,8 +1,173 @@
 import 'package:flutter/material.dart';
+import 'package:flux_software/presentation/core/components/history_card.dart';
 
 class HistoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              HistoryCard(
+                children: [
+                  Text(
+                    'Janeiro',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Litros consumidos:',
+                        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w700, fontSize: 14),
+                      ),
+                      Text(
+                        '5000',
+                        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w700, fontSize: 14),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Total Pago:',
+                        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w700, fontSize: 14),
+                      ),
+                      Text(
+                        'R\$ 47,30',
+                        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w700, fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              HistoryCard(
+                children: [],
+              ),
+              HistoryCard(
+                children: [],
+              ),
+              HistoryCard(
+                children: [
+                  Text(
+                    'Abril',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Litros consumidos:',
+                        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w700, fontSize: 14),
+                      ),
+                      Text(
+                        '5000',
+                        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w700, fontSize: 14),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Total Pago:',
+                        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w700, fontSize: 14),
+                      ),
+                      Text(
+                        'R\$ 47,30',
+                        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w700, fontSize: 14),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Média Semanal:',
+                        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w700, fontSize: 14),
+                      ),
+                      Text(
+                        'R\$ 11,82',
+                        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w700, fontSize: 14),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Média Diária:',
+                        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w700, fontSize: 14),
+                      ),
+                      Text(
+                        'R\$ 1,57',
+                        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w700, fontSize: 14),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 6),
+                    width: double.infinity,
+                    height: 1,
+                    color: Colors.black,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Dia com maior gasto:',
+                        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w700, fontSize: 14),
+                      ),
+                      Text(
+                        '17',
+                        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w700, fontSize: 14),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Litros consumidos',
+                        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w700, fontSize: 14),
+                      ),
+                      Text(
+                        '560',
+                        style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w700, fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              HistoryCard(
+                children: [],
+              ),
+              HistoryCard(
+                children: [],
+              ),
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
