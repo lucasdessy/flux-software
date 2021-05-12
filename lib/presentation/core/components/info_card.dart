2cbbd6c97dcc5de2f3bec9b6114f0bb833e79e41
lucasdessy <lucasedu49@gmail.com>
2021-05-12 13:39:16 -0300

    added flux info card

---
+++ lib/presentation/core/components/info_card.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FluxInfoCard extends StatelessWidget {
+  final String text;
+
+  const FluxInfoCard(this.text, {Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(8),
+      padding: EdgeInsets.only(left: 15, bottom: 8, top: 8),
+      width: double.infinity,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50), color: Colors.white),
+      child: Text(text),
+    );
+  }
+}
