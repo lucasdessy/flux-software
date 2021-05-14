7eb0352173b93c28311e91c45dca486259334775
lucasdessy <lucasedu49@gmail.com>
2021-05-14 13:41:44 -0300

    add description to fluxinfocard

--- lib/presentation/core/components/info_card.dart
+++ lib/presentation/core/components/info_card.dart
@@ -2,22 +2,35 @@ import 'package:flutter/material.dart';
 
 class FluxInfoCard extends StatelessWidget {
   final String text;
-
-  const FluxInfoCard(this.text, {Key? key}) : super(key: key);
+  final String? description;
+  const FluxInfoCard(this.text, {Key? key, this.description}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(8),
-      padding: EdgeInsets.only(left: 15, bottom: 8, top: 8),
+      padding: EdgeInsets.only(left: 15, bottom: 8, top: 8, right: 15),
       width: double.infinity,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(50), color: Colors.white),
-      child: Text(
-        text,
-        style: Theme.of(context)
-            .textTheme
-            .bodyText1
-            ?.copyWith(fontWeight: FontWeight.w700, fontSize: 14),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            text,
+            style: Theme.of(context)
+                .textTheme
+                .bodyText1
+                ?.copyWith(fontWeight: FontWeight.w700, fontSize: 14),
+          ),
+          if (description != null)
+            Text(
+              description!,
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyText1
+                  ?.copyWith(fontWeight: FontWeight.w700, fontSize: 14),
+            ),
+        ],
       ),
     );
   }
