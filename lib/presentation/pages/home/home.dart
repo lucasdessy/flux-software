8b67aff56361ce925c3bd417cf80bd1c74bd3333
lucasdessy <lucasedu49@gmail.com>
2021-05-14 13:49:11 -0300

    fix insets

--- lib/presentation/pages/home/home.dart
+++ lib/presentation/pages/home/home.dart
@@ -1,43 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flux_software/application/bloc/settings_bloc.dart';
-import 'package:flux_software/domain/core/settings.dart';
 import 'package:flux_software/presentation/core/components/info_card.dart';
 import 'package:flux_software/presentation/core/components/progress_indicator.dart';
+import 'package:flux_software/util/region_money_calculator.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<SettingsBloc, SettingsState>(builder: (context, state) {
-      return Column(
-        children: [
-          Expanded(
-            child: FluxProgressIndicator(
-              currentValue: 960,
-              maxValue: state.settings.limit,
+      return Scaffold(
+        body: Center(
+          child: FluxProgressIndicator(
+            currentValue: 960,
+            maxValue: state.settings.limit,
+          ),
+        ),
+        bottomNavigationBar: Container(
+          color: Colors.grey,
+          padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewPadding.bottom),
+          child: Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(16),
+                ),
+                color: Colors.grey.shade400),
+            padding: EdgeInsets.only(
+              top: 10,
+            ),
+            margin: EdgeInsets.all(10),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                FluxInfoCard('Total Consumido:'),
+                FluxInfoCard('Média Mensal:'),
+                FluxInfoCard(
+                  'Estimativa R\$:',
+                  description:
+                      '${RegionMoneyCalculator.formatValue(5000, state.settings.region)}',
+                ),
+              ],
             ),
           ),
-          Container(
-            color: Colors.grey,
-            child: Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(16),
-                  ),
-                  color: Colors.grey.shade400),
-              padding: EdgeInsets.only(top: 10, bottom: 10),
-              margin: EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  FluxInfoCard('Total Consumido:'),
-                  FluxInfoCard('Média Mensal:'),
-                  FluxInfoCard('Estimativa R\$:'),
-                ],
-              ),
-            ),
-          ),
-        ],
+        ),
       );
     });
   }
