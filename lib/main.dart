341afd2e0b1490796ee1ada9e921579591bbefc1
Lucas Dessy <lucasedu49@gmail.com>
2021-05-31 09:46:37 -0300

    configure hydrated bloc storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flux_software/injection.dart';
 import 'package:flux_software/presentation/core/app.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:injectable/injectable.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() async {
   await initializeDateFormatting('pt_BR');
   configureInjection(Environment.prod);
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: await getApplicationDocumentsDirectory(),
+  );
   runApp(App());
 }
